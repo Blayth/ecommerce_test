7e73cc22049e77100a0fa7177ab15d38487f4622
Blayth <58435589+Blayth@users.noreply.github.com>
2020-12-05 18:18:38 +0100

    Ho ripassato un po' typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,113 @@
+/*Questa classe Typescript definisce il comportamento del selettore
+app root che costituisce l'interfaccia principale della nostra applicazione.
+Questo componente viene importato direttamente nell'app.module.ts
+*/
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({//decorator per dire ad angulare che questa classe typescript è un componente
+  selector: 'app-root', //qui diamo il nome al selettore del nostro componente, il nome è facoltativo
+  templateUrl: './app.component.html',/*il link al file html utilizzato per la view del componente,
+  template invece che templateUrl possiamo inserire del codice html all'interno dei backtick ----> ``
+  */
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecommerce';
+
+export class AppComponent {//export riguarda lo scope della classe e fa si che i suoi metodi possano essere utilizzati al di fuori di essa
+  myName: string = "Lorenzo"; //a differenza di javascript standard, con typescript è possibile tipizzare le proprietà
+  mySurname: string = "Leoni"; //queste non sono le classiche variabili javascript ma sono delle proprietà che andiamo a definire
+  myAge: number = 24;// mettendo let davanti a queste proprietà avremo un errore, confermandoci il fatto che non si tratta di variabili
+
+  displayText = false
+  toggle() {
+    console.log(this.displayText)
+    this.displayText = !this.displayText
+  }
+
+  dangerous: boolean = true;
+  componentStyle = {
+    'width': '200px',
+    'height': '100px',
+    'background-color': this.dangerous ? 'red' : 'green'
+  };
+
+  loaded = true;
+  important = true;
+  attention = false;
+  componentClasses = {
+    show: this.loaded, //true
+    warning: this.important && this.attention //false
+  };
+
+  SolaLettura: boolean = true
+
+  elencoArticoli: any[] = [
+    { 'titolo': 'Titolo1', 'autore': 'Autore1' },
+    { 'titolo': 'Titolo2', 'autore': 'Autore2' },
+    { 'titolo': 'Titolo3', 'autore': 'Autore3' }
+  ]
+
+  mammifero: any = {
+    life: "gravidanza",
+    air: "respira",
+    sex: "male & female",
+    type: 4000,
+    uman: true,
+    creation: function(){
+      this.creazione = "Somthing think the Umanity is a big God's Mistake"
+    },
+    famousSpeces: ["uman", "dog", "cats", "yourMom"]
+  }
+
+
+  getName(){
+    return this.myName
+  }
+
+  myFavorite;
+  OnClickMe(){
+    return this.myFavorite = "Stronzo"
+  }
+
+  scritto: string ="" //values rappresenta il valore del testo visualizzato
+  onKey(event: any){
+  this.scritto += event.key; //accumula le battute e riproduce il contenuto digitato
+  }
+  constructor() {
+  }
+
 }
+
+class Courses {
+  constructor(
+  public id: number,
+  public name: string) { }
+  }
+
+//Definiamo una classe più articolata
+class Prova {
+  //alcune proprietà
+  myName:string ="Antonio";
+  myLastName:string ="Giannasca";
+  myMessage:string="";
+  corsi: any[] = [ //è un array di oggetti, che vengono direttamente istanziati e valorizzati (vedi nozione di costruttore)
+  new Courses(0, 'Yii2'),//sto istanziando un nuovo oggetto di classeCourses
+  new Courses(1, 'Node'),
+  new Courses(2, 'Angular'),
+  new Courses(3, 'Ajax')
+  ];
+  //metodo addCorso
+  addCorso(newCorso: string) {
+  if (newCorso) {
+  this.corsi.push(new Courses(this.corsi.length + 1,
+  newCorso));
+  console.log(this.corsi)
+  this.myMessage="";
+  }else{
+  this.myMessage="Dati mancanti";
+  }
+  }
+  }//fine class
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+/*
+Quando il main.ts parte (eseguendo il comando ng serve)
+richiama l'appModule che gli fornisce tutto quello di cui
+ha bisogno per eseguire l'applicazione.
+Di fatti questa classe definisce il bootstrap
+(che non ha niente a che vedere con lo stile)
+e lo passa al main
+*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-@NgModule({
+@NgModule({ //questo dichiarato con la @ è un decorator che serve per far capire ad Angular la funzione di quella classe
   declarations: [
-    AppComponent
+    AppComponent //abbiamo importato direttamente l'app.component.ts
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [//imports definisce le classi che devono essere importate per il funzionamento del component template
+    BrowserModule, //si usa per eseguire l'app su un Browser
+    AppRoutingModule // si usa per gestire il routing ossia stabilire dove inviare un dato elemento di una cominicazione
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//in questo caso gli stiamo dicendo che questa è la classe root, da cui deve partire la lettura dell'applicazione
 })
 export class AppModule { }
